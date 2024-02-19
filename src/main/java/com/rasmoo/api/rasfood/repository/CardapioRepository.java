75fd6ceca038be7ad3f5c9590c6b0cfdbcf69469
Rafael <rafaelteicas@gmail.com>
2024-02-19 08:40:14 -0300

    modify queries

--- src/main/java/com/rasmoo/api/rasfood/repository/CardapioRepository.java
+++ src/main/java/com/rasmoo/api/rasfood/repository/CardapioRepository.java
@@ -3,7 +3,9 @@ package com.rasmoo.api.rasfood.repository;
 import com.rasmoo.api.rasfood.dto.CardapioDto;
 import com.rasmoo.api.rasfood.entity.Cardapio;
 import com.rasmoo.api.rasfood.repository.projection.CardapioProjection;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -24,4 +26,13 @@ public interface CardapioRepository extends JpaRepository<Cardapio, Integer> {
             "INNER JOIN categorias cat on c.categoria_id = cat.id" +
             "WHERE c.categoria_id = ?1 AND c.disponivel = true;", nativeQuery = true)
     List<CardapioProjection> findAllByCategoria(final Integer categoria);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Cardapio c SET c.disponivel = " +
+    "CASE c.disponivel " +
+    "WHEN true WHEN false " +
+    "ELSE true AND" +
+    "WHERE c.id = :id")
+    Integer updateDisponibilidade(final Integer id);
 }
--- src/test/java/com/rasmoo/api/rasfood/RasfoodApiApplicationTests.java
+++ src/test/java/com/rasmoo/api/rasfood/RasfoodApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.rasmoo.api.rasfood;
 
+import com.rasmoo.api.rasfood.repository.CardapioRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RasfoodApiApplicationTests {
 
+	@Autowired
+	private CardapioRepository cardapioRepository;
+
 	@Test
 	void contextLoads() {
+
 	}
 
 }
